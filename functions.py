902c51b8ce74eff0376e9fc58bd695cacf0be04e
khoa_nguyen <johnais207@gmail.com>
2021-07-21 20:15:35 +0700

    takes columns and values from the column from a cvs and pastes them into individual nametags!

--- functions.py
+++ functions.py
@@ -14,52 +14,92 @@ from io import BytesIO
 import pandas as pd
 
 global fname
-global csv
+global read_csv_file
+
+def extract_input(message):
+    msg = message
+    if "<" in msg:
+        start = "<"
+        end = ">"
+        value_names = msg.split(">")
+        value_names.pop()
+        for i in range(len(value_names)):
+            if value_names[i].startswith(" "):
+                value_names[i] = value_names[i][1:]
+            value_names[i] = value_names[i].replace("<", "")
+    else:
+        value_names.append(msg)
+    return value_names
 
 def open_csv(root):
-    global csv
+    global read_csv_file
     csv = filedialog.askopenfile(mode='r',title='Choose a file')
     column_names = []
     csv_string = "Available values: "
-    read_file = pd.read_csv(csv)
-    for col_name in read_file.columns:
+    read_csv_file = pd.read_csv(csv)
+    for col_name in read_csv_file.columns:
         column_names.append(col_name)
         csv_string += col_name + " "
     csv_text = Label(root,text=csv_string)
     csv_text.grid(column = 2, row = 7)
 
 def save_as_pdf(message,x,y):
-    img = Image.open(fname)
-    I1 = ImageDraw.Draw(img)
-    W = int(img.size[0])
-    H = int(img.size[1])
-    ratio = H/W
-    nametag_height = 150
-    nametag_width = int(nametag_height/ratio)
     msg = message
+    c = canvas.Canvas('test.pdf')
 
-
-    myFont = ImageFont.truetype(r'C:\Users\System-Pc\Desktop\timesbd.ttf', 100)
-    w,h = myFont.getsize(msg)
-
-    I1.text(((W-w)/2,(H-h)/2-(185-y)*(150/185)), msg , font = myFont, fill="black")
-    c = canvas.Canvas('ex1.pdf')
     def image_to_byte_array(image:Image):
       imgByteArr = BytesIO()
       image.save(imgByteArr, format=image.format)
       imgByteArr = imgByteArr.getvalue()
       final = ImageReader(BytesIO(imgByteArr))
       return final
-    finalImage = image_to_byte_array(img)
 
-    c.drawImage(finalImage, 50, 30,  nametag_width, nametag_height) #x,y and w,h
-    c.drawImage(finalImage, 50+nametag_width+10, 30,  nametag_width, nametag_height)
+    myFont = ImageFont.truetype(r'C:\Users\System-Pc\Desktop\timesbd.ttf', 100)
 
-    c.drawImage(finalImage, 50, 30+nametag_height+10, nametag_width, nametag_height)
-    c.drawImage(finalImage, 50+nametag_width+10, 30+nametag_height+10,  nametag_width, nametag_height)
+    def clear_canvas():
+        I1.rectangle((100, 100, 300, 150))
 
-    c.drawImage(finalImage, 50, 30+2*nametag_height+20,  nametag_width, nametag_height)
-    c.drawImage(finalImage, 50+nametag_width+10, 30+2*nametag_height+20,  nametag_width, nametag_height)
+    if "<" in msg:
+        user_input_values = extract_input(message)
+        total_array = []
+        global read_csv_file
+        total_array.append(read_csv_file[user_input_values].values)
+        print(total_array)
+        for values_index in range(len(total_array[0])):
+            current_text = ""
+            img = Image.open(fname)
+            I1 = ImageDraw.Draw(img)
+            W = int(img.size[0])
+            H = int(img.size[1])
+            ratio = H/W
+            nametag_height = 150
+            nametag_width = int(nametag_height/ratio)
+            for i in range(len(total_array[0][values_index])):
+                current_text += total_array[0][values_index][i] + " "
+            w,h = myFont.getsize(current_text)
+
+            I1.text(((W-w)/2,(H-h)/2-(185-y)*(150/185)), current_text , font = myFont, fill="black")
+            finalImage = image_to_byte_array(img)
+
+            if (values_index %6 == 0):
+                c.drawImage(finalImage, 50, 30,  nametag_width, nametag_height) #x,y and w,h
+                clear_canvas()
+            if (values_index %6 == 1):
+                c.drawImage(finalImage, 50+nametag_width+10, 30,  nametag_width, nametag_height)
+                clear_canvas()
+            if (values_index %6 == 2):
+                c.drawImage(finalImage, 50, 30+nametag_height+10, nametag_width, nametag_height)
+                clear_canvas()
+            if (values_index %6 == 3):
+                c.drawImage(finalImage, 50+nametag_width+10, 30+nametag_height+10,  nametag_width, nametag_height)
+                clear_canvas()
+            if (values_index %6 == 4):
+                c.drawImage(finalImage, 50, 30+2*nametag_height+20,  nametag_width, nametag_height)
+                clear_canvas()
+            if (values_index %6 == 5):
+                c.drawImage(finalImage, 50+nametag_width+10, 30+2*nametag_height+20,  nametag_width, nametag_height)
+                clear_canvas()
+                c.showPage()
 
     c.showPage()
     c.save()
