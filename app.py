aa7212b795974e4d7d2f71c323e80d16590f7ec9
khoa_nguyen <johnais207@gmail.com>
2021-10-19 05:10:38 +0700

    completed functionality for labels (both previewing and exporting)

--- app.py
+++ app.py
@@ -3,67 +3,152 @@ from tkinter import *
 from tkinter import ttk
 from PIL import Image
 from PIL import ImageDraw
-from functions import save_as_pdf,open_imagefile,open_csv
+from functions import nt_save_as_pdf,nt_open_imagefile,lbl_save_as_pdf,lbl_open_imagefile,open_csv
 import os
 
 
 root = tk.Tk()
 root.geometry('+%d+%d'%(350,10))
+update_in_progress = False
+
+
 
 #interactables frame
 interactables = Frame(root,height=200,width=200)
-interactables.grid(columnspan=3, rowspan=10, row=0,column=0)
+interactables.grid(columnspan=4, rowspan=10, row=0,column=0)
+
+
 
 #display frame
 display = Frame(root,height = 200,width=200)
-display.grid(columnspan = 2, rowspan = 2,row=4,column=6)
+display.grid(columnspan = 2, rowspan = 10,row=0,column=4)
 
 #options menu (checklist format)
 options_list = ["Nametag", "Label", "Certificate"]
 format_option = tk.StringVar(interactables)
 format_option.set("Select an Option")
 question_menu = tk.OptionMenu(interactables, format_option, *options_list)
-
-#labels
-name_label = Label(interactables, text="Text: ", font=("Raleway", 10))
-x_position_label = Label(interactables, text="X Position: ", font=("Raleway", 10))
-y_position_label = Label(interactables, text="Y Position: ", font=("Raleway", 10))
-
-#text input
-name = tk.StringVar()
-nameEntered = ttk.Entry(interactables, width = 15, textvariable = name)
-
-#open csv button
-csv_btn = Button(interactables,text = "Open CSV", command= lambda:open_csv(interactables),font = "Arial", bg = "#20bebe",fg = "white",height = 1,width=15)
-
-#x potition text input
-x_position = tk.StringVar(value = "150")
-x_position = ttk.Entry(interactables, width = 15, textvariable = x_position)
-x_position.insert(END, "150")
-
-#y potition text input
-y_position = tk.StringVar(value = "70")
-y_position = ttk.Entry(interactables, width = 15, textvariable = y_position)
-y_position.insert(END, "70")
-
-#buttons
-browse_btn = Button(interactables, text="Browse", command= lambda:open_imagefile(display,name.get(),x_position.get(),y_position.get()), font=("Raleway",12), bg="#20bebe", fg="white", height=1, width=15)
-save_pdf_btn = Button(interactables,text = "Save As PDF", command= lambda:save_as_pdf(name.get(),int(x_position.get()),int(y_position.get()),format_option.get()),font = "Arial", bg = "#20bebe",fg = "white",height = 1,width=15)
-
-#gridding for interactables frame
 question_menu.grid(column = 2, row = 1)
-nameEntered.grid(column = 2, row = 2)
-x_position.grid(column = 2, row = 3)
-y_position.grid(column = 2, row = 4)
-browse_btn.grid(column=2, row=5,pady=10)
-csv_btn.grid(column = 2, row = 6, pady = 10)
-save_pdf_btn.grid(column = 2, row = 7, pady = 10)
+
+#ELEMENTS FOR NAMETAGS
+#labels
+nt_name_label = Label(interactables, text="Text: ", font=("Raleway", 10))
+nt_x_position_label = Label(interactables, text="X Position: ", font=("Raleway", 10))
+nt_y_position_label = Label(interactables, text="Y Position: ", font=("Raleway", 10))
+#text input
+nt_name = tk.StringVar()
+nt_nameEntered = ttk.Entry(interactables, width = 15, textvariable = nt_name)
+#open csv button
+nt_csv_btn = Button(interactables,text = "Open CSV", command= lambda:open_csv(interactables),font = "Arial", bg = "#20bebe",fg = "white",height = 1,width=15)
+#x potition text input
+nt_x_position = tk.StringVar(value = "150")
+nt_x_position = ttk.Entry(interactables, width = 15, textvariable = nt_x_position)
+nt_x_position.insert(END, "150")
+#y potition text input
+nt_y_position = tk.StringVar(value = "70")
+nt_y_position = ttk.Entry(interactables, width = 15, textvariable = nt_y_position)
+nt_y_position.insert(END, "70")
+#buttons
+nt_browse_btn = Button(interactables, text="Browse", command= lambda:nt_open_imagefile(display,nt_name.get(),nt_x_position.get(),nt_y_position.get()), font=("Raleway",12), bg="#20bebe", fg="white", height=1, width=15)
+nt_save_pdf_btn = Button(interactables,text = "Save As PDF", command= lambda:nt_save_as_pdf(nt_name.get(),int(nt_x_position.get()),int(nt_y_position.get())),font = "Arial", bg = "#20bebe",fg = "white",height = 1,width=15)
+
+#ELEMENTS FOR LABELS
+#labels
+lbl_name_label = Label(interactables, text="Name: ", font=("Raleway", 10))
+lbl_school_label = Label(interactables, text="School: ", font=("Raleway", 10))
+#text input
+lbl_nameEntered = tk.StringVar()
+lbl_nameEntered = ttk.Entry(interactables, width = 15, textvariable = lbl_nameEntered)
+lbl_schoolEntered = tk.StringVar()
+lbl_schoolEntered = ttk.Entry(interactables, width = 15, textvariable = lbl_schoolEntered)
+#open csv button
+lbl_csv_btn = Button(interactables,text = "Open CSV", command= lambda:open_csv(interactables),font = "Arial", bg = "#20bebe",fg = "white",height = 1,width=15)
+#buttons
+lbl_browse_btn = Button(interactables, text="Browse", command= lambda:lbl_open_imagefile(display,lbl_nameEntered.get(),lbl_schoolEntered.get()), font=("Raleway",12), bg="#20bebe", fg="white", height=1, width=15)
+lbl_save_pdf_btn = Button(interactables,text = "Save As PDF", command= lambda:lbl_save_as_pdf(lbl_nameEntered.get(),lbl_schoolEntered.get()),font = "Arial", bg = "#20bebe",fg = "white",height = 1,width=15)
+
+#ELEMENTS FOR CERTIFICATES
+#labels
+ctf_name_label = Label(interactables, text="Text: ", font=("Raleway", 10))
+ctf_x_position_label = Label(interactables, text="X Position: ", font=("Raleway", 10))
+ctf_y_position_label = Label(interactables, text="Y Position: ", font=("Raleway", 10))
+#text input
+ctf_name = tk.StringVar()
+ctf_nameEntered = ttk.Entry(interactables, width = 15, textvariable = ctf_name)
+#open csv button
+ctf_csv_btn = Button(interactables,text = "Open CSV", command= lambda:open_csv(interactables),font = "Arial", bg = "#20bebe",fg = "white",height = 1,width=15)
+#x potition text input
+ctf_x_position = tk.StringVar(value = "150")
+ctf_x_position = ttk.Entry(interactables, width = 15, textvariable = ctf_x_position)
+ctf_x_position.insert(END, "150")
+#y potition text input
+ctf_y_position = tk.StringVar(value = "70")
+ctf_y_position = ttk.Entry(interactables, width = 15, textvariable = ctf_y_position)
+ctf_y_position.insert(END, "70")
+#buttons
+ctf_browse_btn = Button(interactables, text="Browse", command= lambda:open_imagefile(display,ctf_name.get(),ctf_x_position.get(),ctf_y_position.get(),format_option.get()), font=("Raleway",12), bg="#20bebe", fg="white", height=1, width=15)
+ctf_save_pdf_btn = Button(interactables,text = "Save As PDF", command= lambda:save_as_pdf(ctf_name.get(),int(ctf_x_position.get()),int(ctf_y_position.get()),format_option.get()),font = "Arial", bg = "#20bebe",fg = "white",height = 1,width=15)
+
+def clear_grid():
+    for widget in interactables.grid_slaves():
+        if int(widget.grid_info()["row"]) > 0:
+            widget.grid_forget()
+
+def update(*args):
+    global update_in_progress
+    if update_in_progress: return
+    try:
+        format_option_temp = format_option.get()
+    except ValueError:
+        return
+
+    if format_option_temp == "Nametag":
+        #gridding for interactables frame
+        clear_grid()
+        question_menu.grid(column = 2, row = 1)
+        nt_nameEntered.grid(column = 2, row = 2)
+        nt_x_position.grid(column = 2, row = 3)
+        nt_y_position.grid(column = 2, row = 4)
+        nt_browse_btn.grid(column=2, row=5,pady=10)
+        nt_csv_btn.grid(column = 2, row = 6, pady = 10)
+        nt_save_pdf_btn.grid(column = 2, row = 7, pady = 10)
+        #gridding for labels
+        nt_name_label.grid(column = 1,row=2)
+        nt_x_position_label.grid(column = 1,row=3)
+        nt_y_position_label.grid(column = 1,row=4)
+
+    if format_option_temp == "Label":
+        #gridding for interactables frame
+        clear_grid()
+        question_menu.grid(column = 2, row = 1)
+        lbl_nameEntered.grid(column = 2, row = 2)
+        lbl_schoolEntered.grid(column = 2, row = 3)
+        lbl_browse_btn.grid(column=2, row=4,pady=10)
+        lbl_csv_btn.grid(column = 2, row = 5, pady = 10)
+        lbl_save_pdf_btn.grid(column = 2, row = 6, pady = 10)
+        #gridding for labels
+        lbl_name_label.grid(column = 1,row=2)
+        lbl_school_label.grid(column = 1,row=3)
+
+    if format_option_temp == "Certificate":
+        #gridding for interactables frame
+        clear_grid()
+        question_menu.grid(column = 2, row = 1)
+        ctf_nameEntered.grid(column = 2, row = 2)
+        ctf_x_position.grid(column = 2, row = 3)
+        ctf_y_position.grid(column = 2, row = 4)
+        ctf_browse_btn.grid(column=2, row=5,pady=10)
+        ctf_csv_btn.grid(column = 2, row = 6, pady = 10)
+        ctf_save_pdf_btn.grid(column = 2, row = 7, pady = 10)
+        #gridding for labels
+        ctf_name_label.grid(column = 1,row=2)
+        ctf_x_position_label.grid(column = 1,row=3)
+        ctf_y_position_label.grid(column = 1,row=4)
 
 
-#gridding for labels
-name_label.grid(column = 1,row=2)
-x_position_label.grid(column = 1,row=3)
-y_position_label.grid(column = 1,row=4)
+format_option.trace_add('write', update)
+
+
 
 
 root.mainloop()
--- functions.py
+++ functions.py
@@ -16,6 +16,7 @@ import pandas as pd
 global fname
 global read_csv_file
 
+#GENERAL FUNCTIONS
 #function for extracting input from Text (inside <> e.g. <first name> will extract first name)
 def extract_input(message):
     msg = message
@@ -31,7 +32,15 @@ def extract_input(message):
     else:
         value_names.append(msg)
     return value_names
-
+#local function for resizing image to fit preview (not used to actually resize image)
+def resize_image(img):
+    width = int(img.size[0])
+    height = int(img.size[1])
+    ratio = height/width
+    height = 185
+    width = int(height/ratio)
+    img = img.resize((width,height))
+    return img
 #function for openning csv
 def open_csv(root):
     global read_csv_file
@@ -45,8 +54,9 @@ def open_csv(root):
     csv_text = Label(root,text=csv_string)
     csv_text.grid(column = 2, row = 8)
 
-#function for savepdf button
-def save_as_pdf(message,x,y, format_option):
+
+#NAMETAG FUNCTIONS
+def nt_save_as_pdf(message,x,y):
     msg = message
     c = canvas.Canvas('test.pdf')
 
@@ -72,7 +82,7 @@ def save_as_pdf(message,x,y, format_option):
         total_array = []
         total_array.append(message)
 
-    if format_option == "Nametag":
+        if "<" in msg:
             for values_index in range(len(total_array[0])):
                 current_text = ""
                 img = Image.open(fname)
@@ -108,15 +118,22 @@ def save_as_pdf(message,x,y, format_option):
                     c.drawImage(finalImage, 50+nametag_width+10, 30+2*nametag_height+20,  nametag_width, nametag_height)
                     clear_canvas()
                     c.showPage()
-
-
-
-    if format_option != "Select an Option":
-        c.showPage()
-        c.save()
-
-#open image file for preview
-def open_imagefile(root,message,x,y):
+        else:
+            current_text = ""
+            img = Image.open(fname)
+            I1 = ImageDraw.Draw(img)
+            W = int(img.size[0])
+            H = int(img.size[1])
+            ratio = H/W
+            nametag_height = 150
+            nametag_width = int(nametag_height/ratio)
+            current_text = total_array[0]
+            w,h = myFont.getsize(current_text)
+            I1.text(((W-w)/2,(H-h)/2-(185-y)*(150/185)), current_text , font = myFont, fill="black")
+            finalImage = image_to_byte_array(img)
+            c.drawImage(finalImage, 50, 30,  nametag_width, nametag_height) #x,y and w,h
+    c.save()
+def nt_open_imagefile(display,message,x,y):
     global canvas_img
     global fname
     currdir = os.getcwd()
@@ -124,18 +141,111 @@ def open_imagefile(root,message,x,y):
     img = Image.open(fname)
     img = resize_image(img)
     canvas_img = ImageTk.PhotoImage(img)
-    canvas = Canvas(root,width=300,height=185)
+    canvas = Canvas(display,width=300,height=185)
     canvas.grid(column = 2, row=1)
     canvas.create_image(0,0,image=canvas_img,anchor='nw')
     canvas.create_text(x,y,fill="black",font="Times 20 bold",
-                        text=message)
+    text=message)
 
-#local function for resizing image to fit preview (not used to actually resize image)
-def resize_image(img):
-    width = int(img.size[0])
-    height = int(img.size[1])
-    ratio = height/width
-    height = 185
-    width = int(height/ratio)
-    img = img.resize((width,height))
-    return img
+#LABEL FUNCTIONS
+def lbl_save_as_pdf(message,school):
+    msg = message
+    sch = school
+    c = canvas.Canvas('test.pdf')
+    def image_to_byte_array(image:Image):
+      imgByteArr = BytesIO()
+      image.save(imgByteArr, format=image.format)
+      imgByteArr = imgByteArr.getvalue()
+      final = ImageReader(BytesIO(imgByteArr))
+      return final
+
+    myFont = ImageFont.truetype(r'C:\Users\System-Pc\Desktop\timesbd.ttf', 30)
+
+    def clear_canvas():
+        I1.rectangle((100, 100, 300, 150))
+
+    if "<" in msg:
+        if "<" in sch:
+            user_input_values = extract_input(message)
+            school_input_values = extract_input(school)
+            temp_name_array = []
+            temp_school_array = []
+            for names in read_csv_file[user_input_values].values:
+                temp_name_array.append(" ".join(names))
+            for schools in read_csv_file[school_input_values].values:
+                temp_school_array.append(schools[0])
+        else:
+            user_input_values = extract_input(message)
+            temp_name_array = []
+            temp_school_array = []
+            for names in read_csv_file[user_input_values].values:
+                temp_name_array.append(" ".join(names))
+                temp_school_array.append(school)
+        column_temp = 0
+        for values_index in range(len(temp_name_array)):
+            name_text = ""
+            school_text = ""
+            img = Image.open(fname)
+            I1 = ImageDraw.Draw(img)
+            W = int(img.size[0])
+            H = int(img.size[1])
+            ratio = H/W
+            label_height = 82
+            label_width = int(label_height/ratio)
+            name_text = temp_name_array[values_index]
+            school_text = temp_school_array[values_index]
+            w_name,h_name = myFont.getsize(name_text)
+            w_school, h_school = myFont.getsize(school_text)
+
+            I1.text(((W-w_name)/2,(H-h_name)/3), name_text , font = myFont, fill="black")
+            I1.text(((W-w_school)/2,(H-h_school)/3*2), school_text, font = myFont, fill="black")
+
+            finalImage = image_to_byte_array(img)
+            if values_index % 3 == 0:
+                c.drawImage(finalImage, 0, label_height*column_temp,  label_width, label_height) #x,y and w,h
+                clear_canvas()
+            if values_index % 3 == 1:
+                c.drawImage(finalImage, label_width, label_height*column_temp,  label_width, label_height) #x,y and w,h
+                clear_canvas()
+            if values_index % 3 == 2:
+                c.drawImage(finalImage, label_width*2, label_height*column_temp,  label_width, label_height) #x,y and w,h
+                column_temp+=1
+                clear_canvas()
+            if values_index %3 ==2 and column_temp == 9:
+                c.showPage()
+                column_temp = 0
+    else:
+        current_text = ""
+        img = Image.open(fname)
+        I1 = ImageDraw.Draw(img)
+        W = int(img.size[0])
+        H = int(img.size[1])
+        ratio = H/W
+        nametag_height = 120
+        nametag_width = int(nametag_height/ratio)
+        current_text = message
+        current_school = school
+        w,h = myFont.getsize(current_text)
+        I1.text(((W-w)/3,(H-h)/3), current_text , font = myFont, fill="black")
+        I1.text(((W-w)/3,(H-h)/3*2), current_school, font = myFont, fill="black")
+        finalImage = image_to_byte_array(img)
+        c.drawImage(finalImage, 50, 30,  nametag_width, nametag_height) #x,y and w,h
+
+    c.save()
+def lbl_open_imagefile(display,message,school):
+    global canvas_img
+    global fname
+    currdir = os.getcwd()
+    fname = "localresources/label.jpg"
+    img = Image.open(fname)
+    img = img.resize((202,100))
+    canvas_img = ImageTk.PhotoImage(img)
+    canvas = Canvas(display,width=300,height=185)
+    canvas.grid(column = 2, row=1)
+    canvas.create_image(50,50,image=canvas_img,anchor='nw')
+    canvas.create_text(155,90,fill="black",font="Times 10 bold",
+    text=message)
+    canvas.create_text(155,110,fill="black",font="Times 10 bold",
+    text=school)
+
+#CERTIFICATE FUNCTIONS
